61f10e2a83ae4af766bb78b0e73b4b49f79251a9
JonasLK <49680607+JonasLK@users.noreply.github.com>
2020-10-09 14:54:25 +0200

    .

---
+++ Metroplex's Last Stand/Assets/Scripts/Towers/Other/MoneyTower.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using UnityEngine;
+
+public class MoneyTower : MonoBehaviour
+{
+    public MoneyManager moneyManager;
+    void Awake()
+    {
+        moneyManager = GetComponent<OnTower>().moneyManager;
+    }
+
+    void Update()
+    {
+        
+    }
+}
