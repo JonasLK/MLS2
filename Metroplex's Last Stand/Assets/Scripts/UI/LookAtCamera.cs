3307b7aa98f2ac02faa33908e4c15671b93e0e5d
JonasLK <49680607+JonasLK@users.noreply.github.com>
2020-10-21 11:42:47 +0200

    .

---
+++ Metroplex's Last Stand/Assets/Scripts/UI/LookAtCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    public GameObject camera;
+
+    void Start()
+    {
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.LookAt(camera.transform);
+    }
+}
